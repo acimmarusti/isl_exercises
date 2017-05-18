4d4f7f707562003aa46d5931b08b8d25b9b265ab
Cimmarusti <andres.d.cimmarusti@intel.com>
2017-05-18 10:45:42 -0700

    Finished chap3 exercise 15

--- chap3/chap3ex15.py
+++ chap3/chap3ex15.py
@@ -22,30 +22,76 @@ data = rawdata.replace(to_replace='None', value=np.nan).copy()
 
 #Non-response columns#
 allcols = list(data.columns)
-allcols.remove('MEDV')
+allcols.remove('CRIM')
 
-allparams = pd.Series(index=allcols)
+#Linear regression for each predictor#
+lin_par = pd.DataFrame(columns=['B1', 'B1p', 'R2', 'Fval', 'Fpval'])
 
 for var in allcols:
 
-    slinreg = smf.ols('MEDV~' + var, data=data).fit()
-    allparams[var] = slinreg.params[var]
+    slinreg = smf.ols('CRIM~' + var, data=data).fit()
+
+    #Saving important fit metrics#
+    lin_par.loc[var, 'B1'] = slinreg.params[var]
+    lin_par.loc[var, 'B1p'] = slinreg.pvalues[var]
+    lin_par.loc[var, 'R2'] = slinreg.rsquared
+    lin_par.loc[var, 'Fval'] = slinreg.fvalue
+    lin_par.loc[var, 'Fpval'] = slinreg.f_pvalue
+
     if slinreg.pvalues[var] > 0.01:
-        print(var + ' fit coefficient has p-value larger than 1%')
+        print('\n')
+        print(var + ' fit coefficient has p-value = ' + str(slinreg.pvalues[var]))
         print(var + ' fit has R2 = ' + str(slinreg.rsquared))
-        print(var + ' fit has F-statistic = ' + str(slinreg.fvalue) + ' with p-value of ' + str(slinreg.f_pvalue))
+        print(var + ' fit has F-statistic = ' + str(slinreg.fvalue) + ' with p-value = ' + str(slinreg.f_pvalue))
+        print('\n')
    
 
-#All-linear regression#
+#Multiple linear regression#
 col_str = '+'.join(allcols)
-alinreg = smf.ols('MEDV~' + col_str, data=data).fit()
-
+alinreg = smf.ols('CRIM~' + col_str, data=data).fit()
 print(alinreg.summary())
+
+#Parameters that reject null hypothesis#
+nnull_par = alinreg.pvalues[alinreg.pvalues < 0.04]
+print('\nParameters that reject null hypothesis')
+print(list(nnull_par.index))
 
 #plot univariate reg coeff vs multi-reg coeff#
 fig, ax = plt.subplots()
-ax.scatter(alinreg.params[allcols], allparams)
+ax.scatter(lin_par['B1'], alinreg.params[allcols])
 ax.set_xlabel('univariate coeff')
 ax.set_ylabel('multiple reg coeff')
 
+#Non-Linear regression for each predictor#
+nlin_par = pd.DataFrame(columns=['R2', 'Fval', 'Fpval'])
+
+for var in allcols:
+
+    mform = 'CRIM ~ ' + var + ' + np.power(' + var + ', 2)' + '+ np.power(' + var + ', 3)'
+    
+    mlinreg = smf.ols(formula=mform, data=data).fit()
+
+    #Saving important fit metrics#
+    nlin_par.loc[var, 'R2'] = mlinreg.rsquared
+    nlin_par.loc[var, 'Fval'] = mlinreg.fvalue
+    nlin_par.loc[var, 'Fpval'] = mlinreg.f_pvalue
+
+    #Show which parameters have high p-values#
+    hi_pval = mlinreg.pvalues[mlinreg.pvalues > 0.04]
+    lo_pval = mlinreg.pvalues[mlinreg.pvalues < 0.04]
+
+    print('\n')
+    if not lo_pval.empty:
+        print(var + ' fit null hypothesis can be rejected for:')
+        print(list(lo_pval.index))
+        
+    if not hi_pval.empty:
+
+        for par in hi_pval.index:
+            print(var + ' fit coefficient ' + par + ' has p-value = ' + str(mlinreg.pvalues[par]))
+        print(var + ' fit has R2 = ' + str(mlinreg.rsquared))
+        print(var + ' fit has F-statistic = ' + str(mlinreg.fvalue) + ' with p-value = ' + str(mlinreg.f_pvalue))
+
+    print('\n')
+        
 plt.show()
