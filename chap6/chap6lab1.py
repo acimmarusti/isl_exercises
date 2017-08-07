bb8590258b61d8d38e287eb7d2962879d36d4873
Andres Cimmarusti <andres@cimmarusti.tk>
2017-08-07 06:53:04 -0700

    Added and tested function to compute all subsets

--- chap6/chap6lab1.py
+++ chap6/chap6lab1.py
@@ -3,13 +3,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy
 import pandas as pd
+import time
+import itertools
 #import seaborn as sns
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split, LeaveOneOut, KFold
 from sklearn.linear_model import LogisticRegression, LinearRegression
 from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score
-import statsmodels.formula.api as smf
-import statsmodels.api as sm
 
 filename = '../Hitters.csv'
 
@@ -32,34 +32,47 @@ print(data.shape)
 
 print(data.dtypes)
 
-def processSubset(data, x=feats, y=output):
+#Only numerical cols#
+colmask = data.dtypes != object
+numcols = colmask.index[colmask == True]
+xcols = list(numcols)
+xcols.remove('Salary')
 
-    X = np.array(data[feats])
-    Y = np.array(data[output])
+
+def processSubset(data, x=[], y=[]):
+
+    X = np.array(data[x])
+    Y = np.array(data[y])
     
     # Initiate logistic regression object
     lreg = LinearRegression()
 
     # Fit model. Let X = matrix of predictors, Y = matrix of variables.
-    lreg_res = logit_clf.fit(X, Y)
+    lreg_res = lreg.fit(X, Y)
 
     #Predicted values for training set
     Y_pred = lreg_res.predict(X)
 
+    #Number of predictors#
+    kpred = float(len(x))
+
+    #Number of data points#
+    ndat = float(len(Y))
+    
     #Residual sum of squares#
     rss = np.sum(np.square(Y_pred - Y))
 
     #total sum of squares#
     tss = np.sum(np.square(Y - np.mean(Y)))
 
+    #R squared#
+    r2 = 1 - rss / tss
+    
+    #Residual standard error#
+    rse = np.sqrt(rss / (ndat - kpred - 1))
+    
     #Variance estimate#
     var_est = np.square(np.std(Y))
-
-    #Number of predictors#
-    kpred = float(len(feats))
-
-    #Number of data points#
-    ndat = float(len(Y))
     
     #Cp#
     cp = (rss + 2 * kpred * var_est) / ndat
@@ -73,4 +86,26 @@ def processSubset(data, x=feats, y=output):
     #Adjusted R^2#
     ar2 = 1 - (rss / (ndat - kpred - 1)) / (tss / (ndat - 1))
 
-    return {""}
+    return {"Vars": ";".join(x), "model": lreg_res, "NumVar": kpred, "RSE": rse, "R2": r2, "Cp": cp, "AIC": aic, "BIC": bic, "AdjR2": ar2}
+
+kvars = 2
+
+tic = time.time()
+
+results = []
+
+for combo in itertools.combinations(xcols, kvars):
+    results.append(processSubset(data, x=list(combo), y='Salary'))
+
+#Wrap everything in DataFrame#
+allmodels = pd.DataFrame(results)
+
+best_kmodel = allmodels.loc[allmodels['R2'].argmax()]
+
+toc = time.time()
+
+print(best_kmodel)
+print(toc-tic)
+
+
+
