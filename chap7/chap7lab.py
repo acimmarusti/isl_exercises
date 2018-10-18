5cdfeb0a042bdf943e865b716cfb883586dd9ba1
Andres Cimmarusti <andres@cimmarusti.tk>
2018-10-18 06:58:47 -0700

    Added logistic regression, but confidence interval calculation is not working yet

--- chap7/chap7lab.py
+++ chap7/chap7lab.py
@@ -30,10 +30,12 @@ print(data.shape)
 
 print(data.dtypes)
 
+#Polynomial regression#
 lreg = smf.ols(formula='wage~age + np.power(age, 2) + np.power(age, 3) + np.power(age, 4)', data=data).fit()
 
 print(lreg.summary())
 
+#Polynomial regression confidence intervals#
 testdata = pd.DataFrame()
 testdata['age'] = np.arange(data['age'].min(), data['age'].max(), dtype=int)
 
@@ -41,19 +43,19 @@ lreg_pred = lreg.get_prediction(testdata, weights=1)
 
 prediction = lreg_pred.summary_frame(alpha=0.05)
 
-testdata[['pred','stderr','mean_ci_lower','mean_ci_upper','obs_ci_lower','obs_ci_upper']] = prediction
+testdata[['lpred','lstderr','lmean_ci_lower','lmean_ci_upper','lobs_ci_lower','lobs_ci_upper']] = prediction
 
+#Plot regression and confidence intervals#
 f, ax = plt.subplots()
 ax.plot(data['age'], data['wage'], 'o')
-ax.plot(testdata['age'], testdata['pred'], 'g-')
+ax.plot(testdata['age'], testdata['lpred'], 'g-')
 
-ax.plot(testdata['age'], testdata['obs_ci_lower'], 'r--')
-ax.plot(testdata['age'], testdata['obs_ci_upper'], 'r--')
-ax.plot(testdata['age'], testdata['mean_ci_lower'], 'b--')
-ax.plot(testdata['age'], testdata['mean_ci_upper'], 'b--')
+ax.plot(testdata['age'], testdata['lobs_ci_lower'], 'r--')
+ax.plot(testdata['age'], testdata['lobs_ci_upper'], 'r--')
+ax.plot(testdata['age'], testdata['lmean_ci_lower'], 'b--')
+ax.plot(testdata['age'], testdata['lmean_ci_upper'], 'b--')
 
 #ANOVA#
-
 lreg1 = smf.ols(formula='wage~age', data=data).fit()
 lreg2 = smf.ols(formula='wage~age + np.power(age, 2)', data=data).fit()
 lreg3 = smf.ols(formula='wage~age + np.power(age, 2) + np.power(age, 3)', data=data).fit()
@@ -73,6 +75,28 @@ anova_table2 = sm.stats.anova_lm(lregew1, lregew2, lregew3)
 
 print('\nANOVA comparing different polynomial fits: wage vs. eduction and age')
 print(anova_table2)
+
+#Logistic regression#
+data['wage250'] = data['wage'] > 250
+
+greg = smf.glm(formula='wage250~age + np.power(age, 2) + np.power(age, 3) + np.power(age, 4)', data=data, family=sm.families.Binomial()).fit()
+
+#Logistic regression confidence intervals#
+greg_pred = greg.get_prediction(testdata, weights=1)
+
+gpred = greg_pred.summary_frame(alpha=0.05)
+
+testdata[['gpred','gstderr','gmean_ci_lower','gmean_ci_upper','gobs_ci_lower','gobs_ci_upper']] = np.exp(gpred) / (1 + np.exp(gpred))
+
+#Plot regression and confidence intervals#
+fg, axg = plt.subplots()
+axg.plot(data['age'], data['wage250'], 'o')
+axg.plot(testdata['age'], testdata['gpred'], 'g-')
+
+axg.plot(testdata['age'], testdata['gobs_ci_lower'], 'r--')
+axg.plot(testdata['age'], testdata['gobs_ci_upper'], 'r--')
+axg.plot(testdata['age'], testdata['gmean_ci_lower'], 'b--')
+axg.plot(testdata['age'], testdata['gmean_ci_upper'], 'b--')
 
 plt.show()
 """
